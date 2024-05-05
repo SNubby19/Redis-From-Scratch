fd227f212efe3de996623259ff6c697a965a9acf
Sujash Nayak <sujash.nayak@gmail.com>
2024-05-05 01:38:03 -0400

    current state of the code is at a state where the server handles only one request at a time but handles series of requests

--- server.cpp
+++ server.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-#include <cassert>
+#include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,22 +11,32 @@
 
 const size_t k_max_msg = 4096;
 
+static void
+msg(const char *msg)
+{
+    fprintf(stderr, "%s\n", msg);
+}
+
+static void die(const char *msg)
+{
+    int err = errno;
+    fprintf(stderr, "[%d] %s\n", err, msg);
+    abort();
+}
+
 static int32_t read_full(int fd, char *buf, size_t n)
 {
     while (n > 0)
     {
         ssize_t rv = read(fd, buf, n);
-
         if (rv <= 0)
         {
-            return -1; // error or unexpected EOF
+            return -1; // error, or unexpected EOF
         }
-
         assert((size_t)rv <= n);
         n -= (size_t)rv;
         buf += rv;
     }
-
     return 0;
 }
 
@@ -38,25 +47,21 @@ static int32_t write_all(int fd, const char *buf, size_t n)
         ssize_t rv = write(fd, buf, n);
         if (rv <= 0)
         {
-            return -1;
+            return -1; // error
         }
         assert((size_t)rv <= n);
         n -= (size_t)rv;
         buf += rv;
     }
-
     return 0;
 }
 
 static int32_t one_request(int connfd)
 {
-    std::cout << "making 1 request" << std::endl;
-
     // 4 bytes header
     char rbuf[4 + k_max_msg + 1];
     errno = 0;
     int32_t err = read_full(connfd, rbuf, 4);
-
     if (err)
     {
         if (errno == 0)
@@ -67,13 +72,11 @@ static int32_t one_request(int connfd)
         {
             msg("read() error");
         }
-
         return err;
     }
 
     uint32_t len = 0;
     memcpy(&len, rbuf, 4); // assume little endian
-
     if (len > k_max_msg)
     {
         msg("too long");
@@ -82,7 +85,6 @@ static int32_t one_request(int connfd)
 
     // request body
     err = read_full(connfd, &rbuf[4], len);
-
     if (err)
     {
         msg("read() error");
@@ -90,8 +92,8 @@ static int32_t one_request(int connfd)
     }
 
     // do something
-    rbuf[len + 4] = '\0';
-    printf("client says %s\n", &rbuf[4]);
+    rbuf[4 + len] = '\0';
+    printf("client says: %s\n", &rbuf[4]);
 
     // reply using the same protocol
     const char reply[] = "world";
@@ -102,49 +104,23 @@ static int32_t one_request(int connfd)
     return write_all(connfd, wbuf, 4 + len);
 }
 
-static void die(const char *msg)
-{
-    int err = errno;
-    fprintf(stderr, "[%d] %s\n", err, msg);
-    abort();
-}
-
-static void msg(const char *msg)
-{
-    fprintf(stderr, "%s\n", msg);
-}
-
-static void do_something(int connfd)
-{
-    char rbuf[64] = {};
-    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
-    if (n < 0)
-    {
-        msg("read() error");
-        return;
-    }
-    printf("client says: %s\n", rbuf);
-
-    char wbuf[] = "world";
-    write(connfd, wbuf, strlen(wbuf));
-}
-
 int main()
 {
-    // fd assings a number in the system kernel that refers to a tcp connection, a port, or a device in the system
-    // the socket funciton is creating the number that will be used to bind and listen for IPV_4 tcp connections
     int fd = socket(AF_INET, SOCK_STREAM, 0);
-    int val = 1;
+    if (fd < 0)
+    {
+        die("socket()");
+    }
 
-    // this configures various aspects of the socket, SO_REUSEADDR is used to allow for the server to reconnect
-    // to the same address after a restart
+    // this is needed for most server applications
+    int val = 1;
     setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
 
+    // bind
     struct sockaddr_in addr = {};
-
     addr.sin_family = AF_INET;
     addr.sin_port = ntohs(1234);
-    addr.sin_addr.s_addr = ntohl(0);
+    addr.sin_addr.s_addr = ntohl(0); // wildcard address 0.0.0.0
     int rv = bind(fd, (const sockaddr *)&addr, sizeof(addr));
     if (rv)
     {
@@ -160,7 +136,7 @@ int main()
 
     while (true)
     {
-        // accept requests
+        // accept
         struct sockaddr_in client_addr = {};
         socklen_t socklen = sizeof(client_addr);
         int connfd = accept(fd, (struct sockaddr *)&client_addr, &socklen);
@@ -169,17 +145,15 @@ int main()
             continue; // error
         }
 
-        // only serve one client at a time
         while (true)
         {
+            // here the server only serves one client connection at once
             int32_t err = one_request(connfd);
-
             if (err)
             {
                 break;
             }
         }
-
         close(connfd);
     }
 
